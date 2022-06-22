24d9647ba9b8adcd5908bb2c8bc7c35b62112d64
Logan <41876727+lnguyen16@users.noreply.github.com>
2022-06-22 05:40:34 -0400

    Update AddBike.js

--- bike-store/src/components/AddBike.js
+++ bike-store/src/components/AddBike.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// This was supposed to be a page where to add more bike products in the application, this is just a filler so far
 const AddBike = () => {
   return <div>This is the Add Bike Page</div>
 };
