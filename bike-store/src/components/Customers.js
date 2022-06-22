1566c559fd5befa9b7f61f415fdfc3ebcf854ccb
Logan <41876727+lnguyen16@users.noreply.github.com>
2022-06-22 05:40:54 -0400

    Update Customers.js

--- bike-store/src/components/Customers.js
+++ bike-store/src/components/Customers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// This was supposed to check the database for the customers, filler page for now
 const Customers = () => {
   return <div>This is the customers page</div>
 };
