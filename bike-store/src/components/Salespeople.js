46fae62c033fca74630816cfc506789b9be50864
Logan <41876727+lnguyen16@users.noreply.github.com>
2022-06-22 05:41:42 -0400

    Update Salespeople.js

--- bike-store/src/components/Salespeople.js
+++ bike-store/src/components/Salespeople.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// This was supposed to be a page for the employees, it is just a filler page for now
 const Salespeople = () => {
   return <div>This is the salespeople page</div>
 };
